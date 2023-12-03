4afd62ab7694745c782f1bb46c35b3cfeb0b02d4
Ryan Ruiming Li <ryan.ruiming.li@gmail.com>
2023-12-03 12:08:55 -0500

    updated ui pwrup
    added yellow power bar to bottom

--- Assets/Ryan/Scripts/PlayerHealth.cs
+++ Assets/Ryan/Scripts/PlayerHealth.cs
@@ -7,17 +7,22 @@ using UnityEngine.SceneManagement;
 public class PlayerHealth : MonoBehaviour
 {
     private float health;
+    private float pwr;
     private float lerpTimer;
     private LogicScript logic;
     public float maxHealth = 100f;
     public float chipSpeed = 2f;
+    public float maxPWR = 100f;
     public Image frontHealthBar;
     public Image backHealthBar;
+    public Image frontPWRBar;
+    public Image backPWRBar;
     // Start is called before the first frame update
     void Start()
     {
     
         health = maxHealth;
+        pwr = 0;
         GameObject logicGameObject = GameObject.FindWithTag("Logic");
         if (logicGameObject == null)
         {
@@ -32,12 +37,24 @@ public class PlayerHealth : MonoBehaviour
     void Update()
     {
         health = Mathf.Clamp(health, 0, maxHealth);
+        pwr = Mathf.Clamp(pwr, 0, maxPWR);
         UpdateHealthUI();
+        UpdatePowerUpBar();
         if (Input.GetKeyDown(KeyCode.Q))
         {
             TakeDamage(Random.Range(5, 10));
         }
         
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RestoreHealth(Random.Range(5, 10));
+        }
+
+        /* if (Input.GetKeyDown(KeyCode.R))
+        {
+            GainPWR(Random.Range(5, 10));
+        } */
+
         if (health == 0)
         {
             Debug.Log("health is 0");
@@ -58,10 +75,48 @@ public class PlayerHealth : MonoBehaviour
             float percentComplete = lerpTimer / chipSpeed;
             backHealthBar.fillAmount = Mathf.Lerp(fillBack, hFraction, percentComplete);
         }
+        if (fillFront < hFraction)
+        {
+            backHealthBar.color = Color.blue;
+            backHealthBar.fillAmount = hFraction;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            frontHealthBar.fillAmount = Mathf.Lerp(fillFront, backHealthBar.fillAmount, percentComplete);
+        }
     }
+    public void UpdatePowerUpBar()
+    {
+        float fillFront = frontPWRBar.fillAmount;
+        float fillBack = backPWRBar.fillAmount;
+        float pFraction = pwr / maxPWR;
+        frontPWRBar.fillAmount += 0.00003f;
+        if (fillFront < pFraction)
+        {
+            backPWRBar.color = Color.green;
+            backPWRBar.fillAmount = pFraction;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            frontPWRBar.fillAmount = Mathf.Lerp(fillFront, backHealthBar.fillAmount, percentComplete);
+        }
+
+    }
+
     public void TakeDamage(float damage)
     {
         health -= damage;
+        lerpTimer = 0f;
+    }
+
+    public void RestoreHealth(float healAmount) 
+    {
+        health += healAmount;
+        lerpTimer = 0f;
+        
+    }
+
+    public void GainPWR(float pwrAmount)
+    {
+        pwr += pwrAmount;
         lerpTimer = 0f;
     }
 
